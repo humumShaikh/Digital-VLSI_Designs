92c98fd109e1a117e3a2dd44310584f1c5c8b2de
H.SHAIKH <humumshaikh0@gmail.com>
2025-10-24 19:35:14 +0530

    Update decoder.v

--- Verilog/Basys3/Counter/decoder.v
+++ Verilog/Basys3/Counter/decoder.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: CWIR3
+// Engineer: copperwire
 // 
 // Create Date: 16.10.2025 10:39:04
 // Design Name: 
 // Module Name: decoder
 // Project Name: 
-// Target Devices: 
+// Target Devices: Basys3    (xc7a35tcpg236-1)
 // Tool Versions: 
-// Description: 
+// Description: Converts the received bcd digits into 7-segment compatible code
 // 
 // Dependencies: 
 // BOARD:   xc7a35tcpg236
