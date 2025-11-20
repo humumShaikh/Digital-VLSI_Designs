f760a1ed4818ffa6c78d36c7f970e34b0bd3da07
H.SHAIKH <humumshaikh0@gmail.com>
2025-11-20 13:14:43 +0530

    Update PROGMEM.v

--- Verilog/Basys3/MCU/MCU_v5/PROGMEM.v
+++ Verilog/Basys3/MCU/MCU_v5/PROGMEM.v
@@ -32,31 +32,31 @@ module PROGMEM(
     
     initial begin
         ROM[0] <= 8'h11;
-        ROM[1] <= 8'h18;
-        ROM[2] <= 8'h0E;
-        ROM[3] <= 8'h18;
-        ROM[4] <= 8'h0C;
-        ROM[5] <= 8'h18;
-        ROM[6] <= 8'h0A;
-        ROM[7] <= 8'h18;
-        ROM[8] <= 8'h11;
-        ROM[9] <= 8'h00;
-        ROM[10] <= 8'h18;
-        ROM[11] <= 8'h07;
-        ROM[12] <= 8'h18;
-        ROM[13] <= 8'h05;
-        ROM[14] <= 8'h18;
-        ROM[15] <= 8'h03;
-        ROM[17] <= 8'h12;
-        ROM[18] <= 8'h01;
-        ROM[19] <= 8'h13;
-        ROM[20] <= 8'h07;
-        ROM[21] <= 8'h14;
-        ROM[22] <= 8'h86;
+        ROM[1] <= 8'h15;
+        ROM[2] <= 8'h99;
+        ROM[3] <= 8'hAB;
+        ROM[4] <= 8'hCD;
+        ROM[5] <= 8'hEF;
+        ROM[6] <= 8'h15;
+        ROM[7] <= 8'h12;
+        ROM[8] <= 8'h34;
+        ROM[9] <= 8'h56;
+        ROM[10] <= 8'h78;
+        ROM[11] <= 8'h17;
+        ROM[12] <= 8'h12;
+        ROM[13] <= 8'h34;
+        ROM[14] <= 8'h56;
+        ROM[15] <= 8'h78;
+        ROM[16] <= 8'h01;
+        ROM[17] <= 8'h00;
+        ROM[18] <= 8'h00;
+        ROM[19] <= 8'h00;
+        ROM[20] <= 8'h00;
+        ROM[21] <= 8'h00;
+        ROM[22] <= 8'h00;
         ROM[23] <= 8'h00;
         ROM[24] <= 8'h00;
         ROM[25] <= 8'h00;
-        ROM[26] <= 8'h00;
     end
     
     always @(posedge clk)
