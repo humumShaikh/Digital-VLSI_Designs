7776a40ab2e8cc6e807d56eb3c0ca53975c4819c
HUMUM <humumshaikh0@gmail.com>
2025-12-02 07:36:48 +0530

    Add files via upload

---
+++ SystemVerilog/structure_memory/struct_mem.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.12.2025 12:42:24
+// Design Name: 
+// Module Name: struct_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+typedef struct {
+    logic [7:0] MEM [0:15];
+}memory;
+
+module struct_mem(
+    input logic clk,
+    input logic write,
+    input logic read,
+    input logic [7:0] write_data,
+    input logic [3:0] address,
+    output logic [7:0] read_data
+    );
+    
+    memory RAM;
+    
+    always_ff @(posedge clk)
+    begin
+        if(write)
+        begin
+            RAM.MEM[address] <= write_data;
+        end
+        
+        else if(read)
+        begin
+            read_data <= RAM.MEM[address];
+        end
+    end
+    
+endmodule
